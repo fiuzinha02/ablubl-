d830a013805018d3a0a048e47b966538b5c9857f
fiuzinha02 <020307jp@gmail.com>
2024-10-08 23:01:20 -0300

    Add files via upload

--- Mais2.java
+++ Mais2.java
@@ -7,23 +7,27 @@ public class Mais2 {
         Scanner sc = new Scanner(System.in);
         int a,b,c;
 
-
+// primeiro pergunta o valor de A
         out.println("De o valor de A");
         a = sc.nextInt();
+        //se for 0 então não faz nada
     if (a==0){
     out.println("Nao e equacao de segundo grau");
     }
+    //MAS se não for então pergunta o valor de B e C
     else {
         System.out.println("De o valor de B");
         b = sc.nextInt();
 
         out.println("De o valor de C");
         c = sc.nextInt();
-
+// faz o calculo de delta que é b²-4*a*c
         int delta = (b * b) - (4 * a * c);
+        // se o valor desse calculo for negativo então diga isso e informe o valor de delta
         if (delta < 0) {
             out.println("A equacao nao possui raizes reais");
             out.println("Delta e igual a "+delta);
+            // Mas se for positivo dar o valor dos dois x
         } else if (delta > 0) {
             double raizDelta = Math.sqrt(delta);
             double x1=(-b-raizDelta)/ (2*a);
@@ -33,6 +37,7 @@ public class Mais2 {
             out.println(x1);
             out.println(x2);
         }
+        // agora se delta for 0 então faça esse calculo
         else {
             double x = -b / (2.0*a);
             out.println("A equacao possui apenas uma raiz real");
--- Nada.java
+++ Nada.java
@@ -8,7 +8,11 @@ public class Nada {
 
         System.out.println("Nome completo");
         n=sc.nextInt();
-
+/**
+ * @param n O nome será atribuido
+ * @param s a escolha entre feminino 1 e masculino 2
+ * @param e o estado civil se é casado solteiro ou viuvo
+ */
         System.out.println("Sexo");
         System.out.println("1-Feminino/2-Masculino");
         s=sc.nextInt();
@@ -16,21 +20,25 @@ public class Nada {
         System.out.println("Estado Civil");
         System.out.println("1-Casado(a)/2-Solteiro(a)/Viuvo(a)");
         e=sc.nextInt();
-
+/**
+ * se s for igual a 1 ou seja feminino
+ * e se e for igual a 1 ou seja casada
+ * pergunta quantos anos é casada
+ * @param a anos casada
+ */
         if (s==1 &&  e==1){
             System.out.println("Quantos anos a senhora é casada?");
             a=sc.nextInt();
         }
-        else if (s==2 &&  e==2){
-            a=0;
-        }
+        // se for viuvo(a) diga meus pesames
         if (e==3){
             System.out.println("Meus pesames");
         }
+        //se os anos de casada são 1 fala isso
         if(a==1){
             System.out.println("Casou recentemente ;)");
         }
-
+sc.close();
     }
 
 
